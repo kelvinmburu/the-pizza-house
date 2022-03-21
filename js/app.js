f1691408c0bdee72e3b5211b44d1ad37e0beaa7c
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-21 04:20:31 +0300

    ft: added alert functionality for phone address and names

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 (function() {
     'use strict';
     window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
       var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
       var validation = Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
           if (form.checkValidity() === false) {
@@ -20,8 +18,6 @@
   // Scroll buttons
 
 var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -31,9 +27,23 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// Get address and Number of pizzas validation
+
+function getAddress() {
+    // Address links are
+
+    name = document.form.validationCustom01.value;
+
+    phonenumber = document.form.phoneNumber.value;
+
+    ptype = document.form.p-type.selectedIndex;
+    ptype = document.form.p-type.options[p-type].value;
+
+    toppingFlavor = document.form.toppingFlavor.selectedIndex;
+    toppingFlavor = document.form.toppingFlavor.options[toppingFlavor].value;
 }
