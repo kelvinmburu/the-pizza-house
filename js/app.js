8cf9175dee9f34e5f6ec2fb0c7db7018d6c445b1
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-21 04:23:13 +0300

    st: formatted and idented all code files for readability

--- js/app.js
+++ js/app.js
@@ -1,35 +1,38 @@
 // Form Validation
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
-      var forms = document.getElementsByClassName('needs-validation');
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
+    window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
     }, false);
-  })();
+})();
 
-  // Scroll buttons
+// Scroll buttons
 
 var mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
+
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 // Get address and Number of pizzas validation
@@ -41,8 +44,8 @@ function getAddress() {
 
     phonenumber = document.form.phoneNumber.value;
 
-    ptype = document.form.p-type.selectedIndex;
-    ptype = document.form.p-type.options[p-type].value;
+    ptype = document.form.p - type.selectedIndex;
+    ptype = document.form.p - type.options[p - type].value;
 
     toppingFlavor = document.form.toppingFlavor.selectedIndex;
     toppingFlavor = document.form.toppingFlavor.options[toppingFlavor].value;
