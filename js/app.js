ac2e7f781df7a05ca74efe3f742d342e51bb0e08
kelvinmburu <mburuhkelvin@gmail.com>
2022-03-24 20:50:09 +0300

    ft: added function for choosing pizza, crust, and topping price

--- js/app.js
+++ js/app.js
@@ -103,7 +103,70 @@ $(document).ready(function() {
 
     $('#add').hide();
     $('#add2').show();
-    $('.submit')
+    $('.submit').show();
+
+    $('#add2').click(function(){
+        pizzaSize = $('#size :selected').text();
+        crustChoice = $('#crust :selected').text();
+        toppingsChoice = $('#toppings :selected').text();
+        grandtotal = totalPrice + totalPrice;
+        console.log(grandPrice);
+        // This should alert grand priceToppingsSmall
+
+        if(pizzaSize == 'Small') {
+            sizePrice = 450;
+        }else if(pizzaSize == 'medium') {
+            sizePrice = 650;
+        }else if(pizzaSize == 'large') {
+            sizePrice = 1800;
+        }
+
+        if(crustChoice == 'Crispy') {
+            crustPrice = 150;
+        } else if(crustChoice == 'Stuffed') {
+            crustPrice = 200;
+        }else if(crustChoice == 'Gluten-free') {
+            crustPrice = 250;
+        }
+
+        if(toppingsChoice == 'Chicken') {
+            toppingsPrice = 150;
+        }else if(toppingsChoice == 'Beef'){
+            toppingsPrice = 250;
+        }else if(toppingsChoice == 'Spicy-Hot') {
+            toppingsPrice = 350;
+        }
+
+        let totalPrice = toal(sizePrice, crustPrice, toppingsPrice);
+
+
+        $('#pizza_table').append(
+            '<tr>' +
+            '<td>' + pizzaSize + ' ' + sizePrice + '</td>' +
+            '<td>' + crustChoice + '' + crustPrice + '</td>' +
+            '<td>' + toppingsChoice + ' ' + toppingsPrice + '</td>' +
+            '<td>' + totalPrice + '</td>' +
+            '</tr>'
+        );
+
+    });
+    $('.submit').click(function() {
+        grandPrice = parseInt(grandPrice + totalPrice);
+        console.log(grandPrice);
+        $('#checkOut').modal('show');
+        $('.modal .total_div').prepend(
+            '<p>' + 'TOTAL: ' + totalPrice + '</p>'
+        );
+    });
+    $('#checkOut_yes').click(function(){
+        $('.grand_div').show();
+        let grandLocation = totalPrice + 200;
+        $('.modal .grand_div').prepend(
+            '<p>' + 'GRAND TOTAL' + grandLocation + '</p>'
+        );
+        
+    })
+
 })
 
 // priceToppingsSmall = {
